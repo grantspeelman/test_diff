c7ffeba448eb4f1d3570c14f412ce0e20d53bf27
Grant Petersen-Speelman <grantp@jobready.com.au>
2018-04-04 09:48:19 +1000

    add stop_on_failure and only_missing options

--- lib/test_diff/build_coverage.rb
+++ lib/test_diff/build_coverage.rb
@@ -2,16 +2,17 @@
 module TestDiff
   # class used to build the coverage file
   class BuildCoverage
-    def initialize(spec_folder, pre_load, continue)
+    def initialize(spec_folder, pre_load, stop_on_failure, only_missing)
       @spec_folder = spec_folder
       @pre_load = pre_load
       @batch_queue = Queue.new
-      @continue = continue
+      @stop_on_failure = stop_on_failure
+      @only_missing = only_missing
     end
 
     def run
-      RunableTests.add_all(@spec_folder, @batch_queue, @continue)
-      CoverageRunner.run(@batch_queue, @pre_load, @continue)
+      RunableTests.add_all(@spec_folder, @batch_queue, @only_missing)
+      CoverageRunner.run(@batch_queue, @pre_load, !@stop_on_failure)
     end
   end
 end
